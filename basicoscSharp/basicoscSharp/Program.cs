935bcd315bd02bfee19b81c04b8031772095ab89
pedrovr81999@gmail.com <pvr81999>
2019-11-30 09:40:25 -0600

    actualizacion de variables y ejemplos

--- basicoscSharp/basicoscSharp/Program.cs
+++ basicoscSharp/basicoscSharp/Program.cs
@@ -11,7 +11,21 @@ namespace basicoscSharp
         static void Main(string[] args)
         {
             Console.WriteLine("PEDRO");
+            int añoActual = Convert.ToInt32("2019");
+            double numeroPI = 3.141592;
+            Console.WriteLine("Valores:{0} -- {1}", añoActual, numeroPI);
+            decimal montoFactura = 94593.34m + 0.07m;
+            float impuestoVenta = 9239.04f;
+            Console.WriteLine("Valores:{0} -- {1}", montoFactura, impuestoVenta);
+            char letraInicial = 'p';
+            string nombreCurso = letraInicial + "rogramacion II";
+            Console.WriteLine("Este es el curso {0}", nombreCurso);
+            bool boolVerdadero = true;
+            bool boolFalso = false;
+            Console.WriteLine("Variables boleanas: {0} y {1}", boolVerdadero, boolFalso);
             Console.ReadLine();
+
+
         }
     }
 }
